7edb5a08e571dc0e255e1f406a73246e11de178c
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-14 21:49:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require('express')
-var cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.get('/getlist', function (req, res) {
-  res.send({Sensor_suhu: [{suhu: 37, waktu: "2023-01-01 11:00"}, {suhu: 38, waktu: "2023-01-02 11:30"}, {suhu: 40, waktu: "2023-01-03 12:00"}]});
+app.get('/sensor/suhu', (req, res) => {
+  const data = [
+    { suhu: 37, waktu: "2023-01-01 11:00" },
+    { suhu: 38, waktu: "2023-01-02 11:30" },
+    { suhu: 40, waktu: "2023-01-03 12:00" }
+  ];
+  res.json(data);
 });
 
-app.listen(port, ()=>{
-  console.log("App running on http://localhost:3000/")
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
