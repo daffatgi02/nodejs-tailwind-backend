259af161e60823832f3724ec23a7cda39a7cdeab
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-14 22:29:49 +0700

    Menambah data sebagai contoh

--- datasuhu.sql
+++ datasuhu.sql
@@ -4,3 +4,7 @@ CREATE TABLE sensor_suhu (
   waktu DATETIME NOT NULL,
   suhu INT NOT NULL
 );
+--Menambah data sebagai contoh;
+INSERT INTO sensor_suhu (waktu, suhu) VALUES ('2021-01-01 11:00', 37);
+INSERT INTO sensor_suhu (waktu, suhu) VALUES ('2021-01-02 11:30', 38);
+INSERT INTO sensor_suhu (waktu, suhu) VALUES ('2021-01-03 12:00', 40);
